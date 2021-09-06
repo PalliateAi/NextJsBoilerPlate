d4192d8886a0078a19c3ef0f8d0c483349aa60ed
Bar Bar Jinks <54543475+PalliateAi@users.noreply.github.com>
2021-09-06 10:47:00 +0100

    Meta Head

--- pages/_app.js
+++ pages/_app.js
@@ -3,11 +3,18 @@ import Layout from "../components/Layout";
 import { Route, Router } from "react-router-dom";
 import { createMemoryHistory } from "history";
 import "../components/Form/Form.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   const history = createMemoryHistory();
   return (
     <Router history={history}>
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
+        ></meta>
+      </Head>
       <Layout>
         <Component {...pageProps} />
       </Layout>
